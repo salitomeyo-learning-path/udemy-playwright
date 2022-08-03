1de46827cf23289a7198d1db8819b0e5b4ba8760
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-08-03 14:32:58 -0500

    feat: selector, assertions, annotations

---
+++ tests/example.spec.js
@@ -0,0 +1,60 @@
+import { test, expect } from '@playwright/test'
+
+test("Simple basic test", async ({ page }) =>{
+    await page.goto("https://www.example.com")
+    const pageTitle = await page.locator("h1")
+
+    await expect(pageTitle).toContainText('Example Domain')
+})
+
+test("Clickin on Elements", async ({ page }) => {
+    await page.goto('http://zero.webappsecurity.com/index.html')
+    await page.click('#signin_button')
+    await page.click('text=Sign in')
+
+    const errorMessage = await page.locator('.alert-error')
+    await expect(errorMessage).toContainText('Login and/or password are wrong.')
+})
+
+test.skip("Selectors", async ({ page }) =>{
+    // text
+    await page.click('text=some text')
+    // css selectors
+    await page.click('button')
+    await page.click('#id')
+    await page.click('.class')
+    // only visible css selector
+    await page.click('.submit-button:visible')
+    // Combinations 
+    await page.click('#username .first')
+    // Xpath
+    await page.click('//button')
+})
+
+test.describe('My first test suite', () => {
+    test.only("Working with inputs", async({ page }) => {
+        await page.goto('http://zero.webappsecurity.com/index.html')
+        await page.click('#signin_button')
+    
+        await page.type('#user_login', 'some username')
+        await page.type('#user_password', 'some password')
+        await page.click('text=Sign in')
+    
+        const errorMessage = await page.locator('.alert-error')
+        await expect(errorMessage).toContainText('Login and/or password are wrong.')
+    })
+    
+    test.only("Assertions", async({ page }) => {
+        await page.goto('https://example.com/')
+        await expect(page).toHaveURL('https://example.com/')
+        await expect(page).toHaveTitle('Example Domain')
+    
+        const element = await page.locator('h1')
+        await expect(element).toBeVisible()
+        await expect(element).toHaveText("Example Domain")
+        await expect(element).toHaveCount(1)
+    
+        const nonExistingElement = await page.locator('h5')
+        await expect(nonExistingElement).not.toBeVisible()
+    })
+})
