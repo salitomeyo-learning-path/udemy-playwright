23898d6da416010d43703978735e9731b8082846
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-08-04 16:36:31 -0500

    feat: e2e login and logout

---
+++ tests/e2e/e2e-login.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test'
+
+test.describe("Login / Logout flow", () => {
+    test.beforeEach(async ({ page }) =>{
+        await page.goto('http://zero.webappsecurity.com/')
+    })
+
+    test.only('Negative Scenario for login', async ({ page }) => {
+        await page.click('#signin_button')
+        await page.type('#user_login', 'wrong username')
+        await page.type('#user_password', 'wrong password')
+        await page.click('text=Sign in')
+
+        const errorMessage = await page.locator('.alert-error')
+        await expect(errorMessage).toContainText('Login and/or password are wrong.')
+    })
+
+    test('Positive Scenario for login + logout', async ({ page }) => {
+        await page.click('#signin_button')
+        await page.type('#user_login', 'username')
+        await page.type('#user_password', 'password')
+        await page.click('text=Sign in')
+
+        const accountSummaryTab = await page.locator('#account_summary_tab')
+        await expect(accountSummaryTab).toBeVisible()
+
+        await page.goto('http://zero.webappsecurity.com/logout.html')
+        await expect(page).toHaveURL('http://zero.webappsecurity.com/index.html')
+    })
+})
