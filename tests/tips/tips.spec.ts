69fbadf87e1959f96afaf0fed5f082366f92c297
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-08-08 11:38:19 -0500

    feat: mouse movement, multiple tabs

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test.describe.only("Tips & Tricks section", () => {
+test.describe("Tips & Tricks section", () => {
     test('TestInfo Object', async ({ page }, testInfo) => {
         await page.goto('https://www.example.com')
         console.log(testInfo.title)
@@ -24,4 +24,24 @@ test.describe.only("Tips & Tricks section", () => {
             await page.waitForTimeout(3000)
         })
     }
+
+    test('Mouse movement simulation', async ({ page }) => {
+        await page.goto('https://www.example.com')
+        await page.mouse.move(0, 0)
+        await page.mouse.down()
+        await page.mouse.move(0, 100)
+        await page.mouse.up()
+    })
+
+    test('Multiple browser tabs', async ({ browser }) => {
+        const context = await browser.newContext()
+        const page1 = await context.newPage()
+        const page2 = await context.newPage()
+        const page3 = await context.newPage()
+
+        await page1.goto('https://www.example.com')
+        await page2.goto('https://www.google.com')
+        await page3.goto('http://zero.webappsecurity.com/index.html')
+        await page1.waitForTimeout(5000)
+    })
 })
