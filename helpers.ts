71ba3ccd66c7b8f66c52503b29ad14bc1545a32d
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-08-11 15:45:55 -0500

    feat: BDD with cucumber

--- helpers.ts
+++
@@ -1,7 +0,0 @@
-export async function loadHomepage(page) {
-    await page.goto('https://example.com/')
-}
-
-export async function assertTitle(page) {
-    await page.waitForSelector('h1')
-}
--- utils/data-helpers.ts
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto')
-
-export async function getRandomNumber() {
-    return Math.floor(Math.random() * 1000 + 1)
-}
-
-export async function getRandomString() {
-    return crypto.randomBytes(20).toString('hex')
-}
